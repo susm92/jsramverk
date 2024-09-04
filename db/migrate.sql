e37d93d919d782942747447d25b3875bf7962c41
susm92 <sucrosmrkovic92@gmail.com>
2024-09-04 09:25:12 +0200

    kommenterat ut rader i migrate.sql

--- db/migrate.sql
+++ db/migrate.sql
@@ -3,12 +3,3 @@ CREATE TABLE IF NOT EXISTS documents (
     content TEXT,
     created_at DATE DEFAULT (datetime('now','localtime'))
 );
-
-
-INSERT INTO documents (title, content)
-VALUES ('test1', 'try it out')
-;
-
-INSERT INTO documents (title, content)
-VALUES ('test2', 'try it out')
-;
